eccf9699098eff505dd617257e8d75ce86908884
Johanna <johanna.sacher@uni-weimar.de>
2021-05-14 15:30:11 +0200

    a few more trees

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,21 +6,14 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform target;
-
     public float smoothing = 0.2f;
     
     //bottom left corner
     public Vector2 minPos;
-    
     //top right corner
     public Vector2 maxPos;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    
     // Update is called once per frame
     void LateUpdate()
     {
@@ -28,13 +21,15 @@ public class CameraController : MonoBehaviour
 
         if (transform.position != target.position)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            var position = transform.position;
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, position.z);
             
             // Set Camera Bounds between bottom left corner and top right corner
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
                 
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            position = Vector3.Lerp(position, targetPosition, smoothing);
+            transform.position = position;
         }
     }
 }
