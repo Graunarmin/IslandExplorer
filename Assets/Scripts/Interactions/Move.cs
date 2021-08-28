1eba61f8a957ae98f1e63c43cc3e31dd9056fb97
Johanna <johanna.sacher@uni-weimar.de>
2021-08-28 20:26:38 +0200

    Moving Rocks is working now

--- Assets/Scripts/Interactions/Move.cs
+++ Assets/Scripts/Interactions/Move.cs
@@ -2,32 +2,70 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Move : Interaction
 {
-
     public Transform playerFeet;
+    [SerializeField] private Tilemap groundTilemap;
+    [SerializeField] private Tilemap collisionTilemap;
+    [SerializeField] private Collider2D objectCollider;
+
     public override void Interact()
     {
-        Vector3 direction = playerFeet.transform.position - transform.position;
-        if (direction.y < 0)
+        MoveObject();
+        base.Interact();
+    }
+
+    private void MoveObject()
+    {
+        Vector3 movePosition = DetermineMoveDirection();
+        
+        if (CanMove(movePosition))
         {
-            Debug.Log("Unterhalb");
+            transform.position += movePosition;
         }
-        else if(direction.y > 0)
+    }
+
+    private Vector3 DetermineMoveDirection()
+    {
+        // https://forum.unity.com/threads/detection-if-player-left-or-right-of-enemy.515074/
+        Vector2 relativePoint = transform.InverseTransformPoint(playerFeet.position);
+        if (relativePoint.x < 0f && Mathf.Abs(relativePoint.x) > Mathf.Abs(relativePoint.y))
         {
-            Debug.Log("Oberhalb");
+            return new Vector3(1f, 0f, 0f);
+        }
+        if(relativePoint.x > 0f && Mathf.Abs(relativePoint.x) > Mathf.Abs(relativePoint.y))
+        {
+            return new Vector3(-1f, 0f, 0f);
         }
         
-        if (direction.x < 0)
+        if (relativePoint.y < 0f && Mathf.Abs(relativePoint.x) < Mathf.Abs(relativePoint.y))
         {
-            Debug.Log("Links");
+            return new Vector3(0f, 1f, 0f);
         }
-        else if(direction.x > 0)
+        if(relativePoint.y > 0f && Mathf.Abs(relativePoint.x) < Mathf.Abs(relativePoint.y))
         {
-            Debug.Log("Rechts");
+            return new Vector3(0f, -1f, 0f);
         }
 
-        base.Interact();
+        return new Vector3(0f, 0f, 0f);
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        RaycastHit2D[] hits = new RaycastHit2D[10];
+        ContactFilter2D filter = new ContactFilter2D() { };
+        int numHits = objectCollider.Cast(direction, filter, hits, 1f);
+        for (int i = 0; i < numHits; i++)
+        {
+            if (!hits[i].collider.isTrigger)
+            {
+                Debug.Log("I can't move there because of " + hits[i].collider.gameObject.name);
+                return false;
+                
+            }
+        }
+        return true;
     }
 }
