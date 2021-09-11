a5f34b2b71c945b17a11362f618af1ad5baa41da
Graunarmin <johanna.sacher@student.uni-halle.de>
2021-09-11 17:26:05 +0200

    Notebook Tabs are clicked on select now

---
+++ Assets/Scripts/UI/Tab.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tab : MonoBehaviour, ISelectHandler
+{
+    public void OnSelect(BaseEventData eventData)
+    {
+        Button button = gameObject.GetComponent<Button>();
+        button.onClick.Invoke();
+    }
+}
