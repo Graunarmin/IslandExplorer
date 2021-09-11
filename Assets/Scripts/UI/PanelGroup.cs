180ccc739c4fcaf324de96b018b36f1f5108a71a
Graunarmin <johanna.sacher@student.uni-halle.de>
2021-09-11 17:16:31 +0200

    improved Notebook UI (Grid Layout)

---
+++ Assets/Scripts/UI/PanelGroup.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelGroup : MonoBehaviour
+{
+    public GameObject[] panels;
+    public int panelIndex;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
